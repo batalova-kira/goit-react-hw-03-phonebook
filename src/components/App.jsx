e6c4f2c6c0523cf0ff8f1f09694f79b5adb278f4
batalova-kira <batalovakira@gmail.com>
2023-10-19 20:58:12 +0300

    bug-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,9 +10,20 @@ export class App extends Component {
   state = {
     contacts: [],
     filter: '',
-    name: '',
-    number: '',
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+    }
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
 
   changeFilter = newFilter => {
     this.setState({
